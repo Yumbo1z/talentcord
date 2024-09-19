ea3cfc84e1c9b26bf7188aa4276ac90ebea988e5
Yuyu <82416698+yuyulbm@users.noreply.github.com>
2024-09-19 15:32:36 +0000

    Update

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const session = require("express-session");
-const axios = require("axios");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
