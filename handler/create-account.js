c32a80e4143397701480a755c18cbd9e465cb92b
Yuyu <82416698+yuyulbm@users.noreply.github.com>
2024-10-23 09:25:36 -0500

    Update

--- handler/create-account.js
+++ handler/create-account.js
@@ -3,7 +3,7 @@ const userSchema = require("../models/user");
 module.exports = {
   name: "create-account",
   run: async (req, res) => {
-    let reqData = req.body.data;
+    let reqData = req.body;
     console.log(reqData)
   },
 };
