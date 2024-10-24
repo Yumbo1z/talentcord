386c89ddc959543651e55c5a2dff60094660ae15
Yuyu <82416698+yuyulbm@users.noreply.github.com>
2024-10-24 08:43:37 -0500

    Update

--- handler/signin.js
+++ handler/signin.js
@@ -1,4 +1,5 @@
 const userSchema = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   name: "signin",
@@ -9,6 +10,19 @@ module.exports = {
     let findUser = await userSchema.findOne({ username: reqData.username });
     if (!findUser || findUser.password !== reqData.password) return res.status(400).json({ message: "Incorrect password or username." });
 
-    res.status(200).json({ message: "Signing In..." });
+    const userObject = {
+      username: findUser.username,
+      password: findUser.password,
+    };
+
+    const token = jwt.sign(userObject, process.env.ACCESS_TOKEN_SECRET);
+
+    let url = new URL("https://talentcord-production.up.railway.app/home");
+    url.searchParams.append("token", token);
+    url.searchParams.append("username", findUser.username);
+    url.searchParams.append("avatar", "https://preview.redd.it/orin-kaenbyou-touhou-11-kasha-v0-rce3ewucbzta1.jpg?width=550&format=pjpg&auto=webp&s=52332d0e2882f5f8543510847b60ba74ea3acf48");
+
+    res.status(200).redirect(url);
+    //res.status(200).json({ message: "Signing In..." });
   },
 };
