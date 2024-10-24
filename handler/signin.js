ecb60621413d49cea2916f713c6a2f02a1f5c581
Yuyu <82416698+yuyulbm@users.noreply.github.com>
2024-10-24 09:04:00 -0500

    Update

--- handler/signin.js
+++ handler/signin.js
@@ -5,10 +5,13 @@ module.exports = {
   name: "signin",
   run: async (req, res) => {
     let reqData = req.body;
-    console.log(reqData)
+    console.log(reqData);
 
     let findUser = await userSchema.findOne({ username: reqData.username });
-    if (!findUser || findUser.password !== reqData.password) return res.status(400).json({ message: "Incorrect password or username." });
+    if (!findUser || findUser.password !== reqData.password)
+      return res
+        .status(400)
+        .json({ message: "Incorrect password or username." });
 
     const userObject = {
       username: findUser.username,
@@ -16,11 +19,14 @@ module.exports = {
     };
 
     const token = jwt.sign(userObject, process.env.ACCESS_TOKEN_SECRET);
-console.log(token)
+    console.log(token);
     let url = new URL("https://talentcord-production.up.railway.app/home");
     url.searchParams.append("token", token);
     url.searchParams.append("username", findUser.username);
-    url.searchParams.append("avatar", "https://preview.redd.it/orin-kaenbyou-touhou-11-kasha-v0-rce3ewucbzta1.jpg?width=550&format=pjpg&auto=webp&s=52332d0e2882f5f8543510847b60ba74ea3acf48");
+    url.searchParams.append(
+      "avatar",
+      "https://preview.redd.it/orin-kaenbyou-touhou-11-kasha-v0-rce3ewucbzta1.jpg?width=550&format=pjpg&auto=webp&s=52332d0e2882f5f8543510847b60ba74ea3acf48"
+    );
 
     res.status(200).redirect(url);
     //res.status(200).json({ message: "Signing In..." });
