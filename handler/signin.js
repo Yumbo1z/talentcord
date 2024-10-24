06dc4bd0f3ac2e8cdfe9047afe17570c6a414911
Yuyu <82416698+yuyulbm@users.noreply.github.com>
2024-10-24 09:25:19 -0500

    Update

--- handler/signin.js
+++ handler/signin.js
@@ -5,7 +5,6 @@ module.exports = {
   name: "signin",
   run: async (req, res) => {
     let reqData = req.body;
-    console.log(reqData);
 
     let findUser = await userSchema.findOne({ username: reqData.username });
     if (!findUser || findUser.password !== reqData.password)
@@ -26,7 +25,6 @@ module.exports = {
       "avatar",
       "https://preview.redd.it/orin-kaenbyou-touhou-11-kasha-v0-rce3ewucbzta1.jpg?width=550&format=pjpg&auto=webp&s=52332d0e2882f5f8543510847b60ba74ea3acf48"
     );
-    console.log(url);
     res.status(200).redirect(url);
     //res.status(200).json({ message: "Signing In..." });
   },
