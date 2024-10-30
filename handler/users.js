3aa0a0bb94b57695313c5b7e63350006b7e25625
Yuyu <82416698+yuyulbm@users.noreply.github.com>
2024-10-30 09:02:38 -0500

    Update

--- handler/users.js
+++ handler/users.js
@@ -1,17 +1,18 @@
 const userSchema = require("../models/user");
 
 module.exports = {
-    name: "users",
-    get: true,
-    run: async (req, res) => {
-        let data = await userSchema.find().sort({ username: 1 });
-        let newObj = data.map((v) => {
-            return {
-                username: v.username,
-                bio: v.bio,
-                tags: v.tags,
-            };
-        });
-        res.send(newObj);
-    },
+  name: "users",
+  get: true,
+  run: async (req, res) => {
+    let data = await userSchema.find().sort({ username: 1 });
+    let newObj = data.map((v) => {
+      return {
+        username: v.username,
+        bio: v.bio,
+        tags: v.tags,
+        portfolio: v.portfolio,
+      };
+    });
+    res.send(newObj);
+  },
 };
