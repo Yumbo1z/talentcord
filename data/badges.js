57f3b681d16c8081885b0fb2b220a21eac5c5aa8
Yuyu <82416698+yuyulbm@users.noreply.github.com>
2024-11-19 07:54:19 -0600

    Update

--- data/badges.js
+++ data/badges.js
@@ -1,6 +1,14 @@
 module.exports = [
-    {
-      name: "OG",
-      image: "https://i.imgur.com/rGsP6oh.gif",
-    },
+  {
+    name: "Translator",
+    image: "https://i.imgur.com/rGsP6oh.gif",
+  },
+  {
+    name: "Developer",
+    image: "https://i.imgur.com/rGsP6oh.gif",
+  },
+  {
+    name: "Moderator",
+    image: "https://i.imgur.com/rGsP6oh.gif",
+  },
 ];
