0965934ea87c90473bcc7eacfd9f159ba2f77bbd
Yuyu <82416698+yuyulbm@users.noreply.github.com>
2024-12-12 11:13:25 -0600

    Update

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get("/", (_, res) => res.redirect(link("home")));
 
 app.get("/home", async (_, res) => res.sendFile(dir("home")));
 
-app.listen(process.env.PORT || 8080, () => {
+app.listen(process.env.PORT || 80, () => {
   console.log("Server Started");
 });
 
