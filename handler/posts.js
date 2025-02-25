96599c4f8127f1971344578764637da5a4e33385
Yumbo1z <82416698+yuyulbm@users.noreply.github.com>
2025-02-24 21:40:38 -0600

    Update

--- handler/posts.js
+++ handler/posts.js
@@ -5,27 +5,30 @@ module.exports = {
   name: "posts",
   get: true,
   run: async (req, res) => {
-    let data = await postShema.find().sort({ date: -1 });
-    let newObj = data.map(async (v) => {
-      let user = await userSchema.findOne({
-        username: v.username,
-      });
+    try {
+      let data = await postShema.find().sort({ date: -1 });
 
-      return {
-        username: v.username,
-        icon: user.avatarURL
-          ? user.avatarURL
-          : "https://i.imgur.com/fBh0Mdm.png",
-        portfolio: user.portfolio,
-        permissions: user.permissions,
-        badges: user.badges,
-        onTop: v.onTop,
-        content: v.content,
-        date: v.date,
-      };
-    });
+      let newObj = await Promise.all(
+        data.map(async (v) => {
+          let user = await userSchema.findOne({ username: v.username });
 
-    console.log(newObj);
-    res.send(newObj);
+          return {
+            username: v.username,
+            icon: user?.avatarURL || "https://i.imgur.com/fBh0Mdm.png",
+            portfolio: user?.portfolio || "",
+            permissions: user?.permissions || [],
+            badges: user?.badges || [],
+            onTop: v.onTop,
+            content: v.content,
+            date: v.date,
+          };
+        })
+      );
+
+      res.send(newObj);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ error: "Internal Server Error" });
+    }
   },
 };
