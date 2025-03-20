83230294ad7d2f1d4e4618d89bbdc780293079cd
Yumbo1z <82416698+yuyulbm@users.noreply.github.com>
2025-03-20 11:21:24 -0500

    update

--- js/profile.js
+++ js/profile.js
@@ -49,3 +49,231 @@ document.addEventListener("DOMContentLoaded", async () => {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Check if user data exists in localStorage
+  const userData = JSON.parse(localStorage.getItem("user"));
+  if (userData) {
+    replaceCrossWithAvatar(userData.avatar); // Replace the cross icon with an avatar
+  }
+
+  // Refresh page if user is signed in
+  if (window.location.search.includes("token")) {
+    const username = new URLSearchParams(window.location.search).get(
+      "username"
+    );
+    const token = new URLSearchParams(window.location.search).get("token");
+    const avatar = new URLSearchParams(window.location.search).get("avatar");
+
+    const user = { username, avatar, token };
+    localStorage.setItem("user", JSON.stringify(user)); // Save user data to localStorage
+    window.location.href = "https://talentcord.org/";
+  }
+});
+
+function showContextMenu(event, card) {
+  const menu = document.getElementById("customContextMenu");
+  menu.style.display = "block";
+  menu.style.left = `${event.pageX}px`;
+  menu.style.top = `${event.pageY}px`;
+  menu.dataset.username = card.dataset.user;
+  menu.dataset.perms = card.dataset.perms;
+}
+
+async function banUser() {
+  const username =
+    document.getElementById("customContextMenu").dataset.username;
+  const targetPerms =
+    document.getElementById("customContextMenu").dataset.perms;
+  const userData = JSON.parse(localStorage.getItem("user"));
+
+  try {
+    const response = await fetch("/ban", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: userData.token,
+        targetUsername: username,
+        targetPerms,
+      }),
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      alert("Successfully banned user!");
+    } else {
+      alert("Failed to banned user: " + result.message);
+    }
+  } catch (error) {
+    console.error("Error during ban:", error);
+    alert("An error occurred. Please try again later.");
+  }
+}
+
+async function timeoutUser() {
+  const username =
+    document.getElementById("customContextMenu").dataset.username;
+  const targetPerms =
+    document.getElementById("customContextMenu").dataset.perms;
+  const userData = JSON.parse(localStorage.getItem("user"));
+
+  try {
+    const response = await fetch("/timeout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: userData.token,
+        targetUsername: username,
+        targetPerms,
+      }),
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      alert("Successfully timeouted user!");
+    } else {
+      alert("Failed to timeout user: " + result.message);
+    }
+  } catch (error) {
+    console.error("Error during timeout:", error);
+    alert("An error occurred. Please try again later.");
+  }
+}
+
+async function givedev() {
+  const username =
+    document.getElementById("customContextMenu").dataset.username;
+  const targetPerms =
+    document.getElementById("customContextMenu").dataset.perms;
+  const userData = JSON.parse(localStorage.getItem("user"));
+
+  try {
+    const response = await fetch("/givedev", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: userData.token,
+        targetUsername: username,
+        targetPerms,
+      }),
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      alert("Success");
+    } else {
+      alert("Failed to award user: " + result.message);
+    }
+  } catch (error) {
+    console.error("Error during awarding:", error);
+    alert("An error occurred. Please try again later.");
+  }
+}
+
+async function givemod() {
+  const username =
+    document.getElementById("customContextMenu").dataset.username;
+  const targetPerms =
+    document.getElementById("customContextMenu").dataset.perms;
+  const userData = JSON.parse(localStorage.getItem("user"));
+
+  try {
+    const response = await fetch("/givemod", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: userData.token,
+        targetUsername: username,
+        targetPerms,
+      }),
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      alert("Success");
+    } else {
+      alert("Failed to award user: " + result.message);
+    }
+  } catch (error) {
+    console.error("Error during awarding:", error);
+    alert("An error occurred. Please try again later.");
+  }
+}
+
+async function giveverified() {
+  const username =
+    document.getElementById("customContextMenu").dataset.username;
+  const targetPerms =
+    document.getElementById("customContextMenu").dataset.perms;
+  const userData = JSON.parse(localStorage.getItem("user"));
+
+  try {
+    const response = await fetch("/giveverified", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: userData.token,
+        targetUsername: username,
+        targetPerms,
+      }),
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      alert("Success");
+    } else {
+      alert("Failed to award user: " + result.message);
+    }
+  } catch (error) {
+    console.error("Error during awarding:", error);
+    alert("An error occurred. Please try again later.");
+  }
+}
+
+async function deletePost() {
+  const username = document.getElementById("postContextMenu").dataset.username;
+  const targetPerms = document.getElementById("postContextMenu").dataset.perms;
+  const postId = document.getElementById("postContextMenu").dataset.id;
+  const userData = JSON.parse(localStorage.getItem("user"));
+
+  try {
+    const response = await fetch("/delete-post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: userData.token,
+        targetUsername: username,
+        targetPerms,
+        id: postId,
+      }),
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      alert("Successfully deleted post!");
+    } else {
+      alert("Failed to delete post: " + result.message);
+    }
+  } catch (error) {
+    console.error("Error during ban:", error);
+    alert("An error occurred. Please try again later.");
+  }
+}
