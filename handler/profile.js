a7b2f008f2ad2d19a3f4206dadcbfa332f29dde0
Yumbo1z <82416698+yuyulbm@users.noreply.github.com>
2025-03-20 12:03:14 -0500

    update

--- handler/profile.js
+++ handler/profile.js
@@ -6,8 +6,6 @@ module.exports = {
   get: true,
   run: async (req, res) => {
     try {
-        console.log(req.cookies)
-      let requestToken = req.cookies.token;
       let username = req.params.username;
       let user = await userSchema.findOne({
         username: username,
@@ -22,43 +20,6 @@ module.exports = {
       let file = fs.readFileSync("./html/profile.html", {
         encoding: "utf8",
       });
-
-      if (requestToken) {
-        jwt.verify(
-          requestToken,
-          process.env.ACCESS_TOKEN_SECRET,
-          (err, user) => {
-            if (err) return res.status(403).json({ error: `Token is invalid` });
-            req.user = user;
-          }
-        );
-
-        let data = await profileShema.findOne({
-          _id: req.user._id,
-        });
-
-        if (!data) return res.status(400).json({ error: `No data found.` });
-
-        const userObject = {
-          username: findUser.username,
-          password: findUser.password,
-        };
-
-        const token = jwt.sign(userObject, process.env.ACCESS_TOKEN_SECRET);
-
-        // Set cookie with a far-future expiration date
-        const expirationDate = new Date();
-        expirationDate.setFullYear(expirationDate.getFullYear() + 1); // Expires in 1 year
-
-        res.cookie(`userToken`, token, {
-          secure: true,
-          httpOnly: true,
-          sameSite: "lax",
-          expires: expirationDate,
-        });
-
-        file = file.replaceAll("$$token$$", token);
-      }
 
       file = file.replaceAll("$$permissions$$", user.permissions);
       file = file.replaceAll("$$username$$", user.username);
