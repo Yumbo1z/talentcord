d045470ba53a604a6894b19300b6696cfaba2807
Yumbo1z <82416698+yuyulbm@users.noreply.github.com>
2025-03-20 11:12:34 -0500

    Update

---
+++ js/profile.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    let response = await fetch("/users");
+    let json = await response.json();
+  
+    const currentUser = JSON.parse(localStorage.getItem("user"));
+  
+    document.querySelectorAll(".listing-card").forEach((card) => {
+      card.addEventListener("contextmenu", (event) => {
+        event.preventDefault();
+        if (currentUser) {
+          let currentUserMoreData = json.find(
+            (v) => v.username === currentUser.username
+          );
+          if (currentUserMoreData.permissions === 1) showContextMenu(event, card);
+        }
+      });
+    });
+  
+    refreshPosts();
+  });
+
+async function refreshPosts() {
+    let response = await fetch("/posts");
+    let json = await response.json();
+    const posts = document.getElementById("posts");
+  
+    // Clear the existing posts before adding new ones
+    posts.innerHTML = "";
+  
+    const currentUser = JSON.parse(localStorage.getItem("user"));
+  
+    for (let post of json) {
+      // Map badges to Font Awesome icons
+      let badges = post.badges
+        .map((badge) => {
+          return `<div class="badge-container">
+                  <i class="${badge.faClass} badge-icon"></i>
+                  <span class="badge-tooltip">${badge.name}</span>
+                </div>`;
+        })
+        .join("");
+  
+      posts.innerHTML += `
+      <div class="post-card" data-user="${post.username}" data-perms="${post.permissions}" data-id="${post._id}">
+        <img src="${post.icon}" alt="User Avatar" class="avatar">
+        <div class="listing-info">
+          <div class="profile-header">
+            <h3><a style="color: white;" href="${post.portfolio}" onclick="confirmExit(event, '${post.portfolio}')" target="_blank">${post.username}</a></h3>
+            <div class="badge-icons">
+              ${badges}
+            </div>
+          </div>
+          <p>${post.content}</p>
+        </div>
+      </div>`;
+    }
+  
+    document.querySelectorAll(".post-card").forEach((card) => {
+      card.addEventListener("contextmenu", (event) => {
+        event.preventDefault();
+        if (currentUser) {
+          let currentUserMoreData = json.find(
+            (v) => v.username === currentUser.username
+          );
+          if (currentUserMoreData.permissions === 1)
+            showPostContextMenu(event, card);
+        }
+      });
+    });
+  }
