acf00be0c2599e3873085f6a1d0055180a35121a
Yuyu <Jesus71236gaming@gmail.com>
2025-05-25 12:54:22 -0500

    Update

--- js/profile.js
+++ js/profile.js
@@ -1,40 +1,5 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  let response = await fetch("/posts");
-  let json = await response.json();
-  const posts = document.getElementById("posts");
-
-  // Clear the existing posts before adding new ones
-  posts.innerHTML = "";
-
   const currentUser = JSON.parse(localStorage.getItem("user"));
-
-  let filteredPosts = json.filter((p) => p.username === currentUser.username);
-
-  for (let post of filteredPosts) {
-    // Map badges to Font Awesome icons
-    let badges = post.badges
-      .map((badge) => {
-        return `<div class="badge-container">
-                  <i class="${badge.faClass} badge-icon"></i>
-                  <span class="badge-tooltip">${badge.name}</span>
-                </div>`;
-      })
-      .join("");
-
-    posts.innerHTML += `
-      <div class="post-card" data-user="${post.username}" data-perms="${post.permissions}" data-id="${post._id}">
-        <img src="${post.icon}" alt="User Avatar" class="avatar">
-        <div class="listing-info">
-          <div class="profile-header">
-            <h3><a style="color: white;" href="${post.portfolio}" onclick="confirmExit(event, '${post.portfolio}')" target="_blank">${post.username}</a></h3>
-            <div class="badge-icons">
-              ${badges}
-            </div>
-          </div>
-          <p>${post.content}</p>
-        </div>
-      </div>`;
-  }
 
   let profile = document.getElementById("profile");
 
